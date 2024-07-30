a92f23ee666538e0f9dc9e5b635529382927918d
Shiva Kalam <shivakalam1016@gmail.com>
2024-07-30 16:34:30 -0400

    fix: styling of tables and ui issues

--- src/components/table.jsx
+++ src/components/table.jsx
@@ -2,9 +2,9 @@ import { useState } from "react";
 
 // Sample Data for demonstration purposes
 let data = [
-    {name: 'Jack Jackson', email: 'jackj@abc.com', password: 'jackj'},
-    {name: 'Katie Kates', email: 'katiek@abc.com', password: 'katiek'},
-    {name: 'Glen Glenns', email: 'gleng@abc.com', password: 'gleng'}
+    {name: 'Jack Jackson', email: 'jackj@abc.com', password: 'jackj', id: 1},
+    {name: 'Katie Kates', email: 'katiek@abc.com', password: 'katiek', id: 2},
+    {name: 'Glen Glenns', email: 'gleng@abc.com', password: 'gleng', id: 3}
 ]
 
 export const Table = () => {
@@ -29,56 +29,54 @@ export const Table = () => {
     }
 
     return (
-        <form name="Customer List">
-            <fieldset>
-                <table>
-                    <thead>
-                        <tr><th>Customer List</th></tr>
-                    </thead>
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Pass</th>
-                        </tr>
-                    </thead>
-                    <tbody onClick={handleListClick}>
-                        {data.map((entry, index) => (
-                            <tr key={index}>
-                                <td>{entry.name}</td>
-                                <td>{entry.email}</td>
-                                <td>{entry.password}</td>
+        <div>
+            <div className="boxed">
+                <h4>Customer List</h4>
+                    <table id="customer-list">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Pass</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
-                <table>
-                    <thead>
-                        <tr><th>Update</th></tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td><label htmlFor="#name">Name:</label></td>
-                            <td><input id="name" type="text"/></td>
-                        </tr>
-                        <tr>
-                            <td><label htmlFor="#email">Email:</label></td>
-                            <td><input id="email" type="email"/></td>
-                        </tr>
-                        <tr>
-                            <td><label htmlFor="#pass">Pass:</label></td>
-                            <td><input id="pass" type="password"/></td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <button type="button" onClick={onDeleteClick}>Delete</button>
-                                <button type="button" onClick={onSaveClick}>Save</button>
-                                <button type="button" onClick={onCancelClick}>Cancel</button>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </fieldset>
-        </form>
+                        </thead>
+                        <tbody onClick={handleListClick}>
+                            {data.map((entry, index) => (
+                                <tr key={index}>
+                                    <td>{entry.name}</td>
+                                    <td>{entry.email}</td>
+                                    <td>{entry.password}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+                <div className="boxed">
+                    <h4>Update</h4>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td><label htmlFor="#name">Name:</label></td>
+                                <td><input id="name" type="text"/></td>
+                            </tr>
+                            <tr>
+                                <td><label htmlFor="#email">Email:</label></td>
+                                <td><input id="email" type="email"/></td>
+                            </tr>
+                            <tr>
+                                <td><label htmlFor="#pass">Pass:</label></td>
+                                <td><input id="pass" type="password"/></td>
+                            </tr>
+                            <tr>
+                                <td colSpan={2}>
+                                    <button type="button" onClick={onDeleteClick}>Delete</button>
+                                    <button type="button" onClick={onSaveClick}>Save</button>
+                                    <button type="button" onClick={onCancelClick}>Cancel</button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
     )
 }
